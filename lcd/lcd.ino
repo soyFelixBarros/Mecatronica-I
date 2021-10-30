92aa9f2d0601c1acf6d9f0e3fe14d7574b0895f3
Felix Barros <soyFelixBarros@gmail.com>
2021-10-30 19:10:40 -0300

    comentamos el código

--- lcd/lcd.ino
+++ lcd/lcd.ino
@@ -1,13 +1,15 @@
 #include <LiquidCrystal.h>
 
+// Inicializar la librería con los números de los pines de comunicación
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
-  lcd.begin(16, 2);
-  lcd.print("Hola Mundo!!");
+  lcd.begin(16, 2);              // Inicia un LCD 16x02 (columnas, filas)
+  lcd.setCursor(0, 0);           // Coloca el cursor en la primera fila
+  lcd.print("Hola Mundo!");      // Imprime un mensaje en la pantalla LCD.
 }
 
 void loop() {
-  lcd.setCursor(0, 1);
-  lcd.print(millis() / 1000);
+  lcd.setCursor(0, 1);           // Coloca el cursor en la segunda fila
+  lcd.print(millis() / 1000);    // Imprime el número de segundos desde el reinicio
 }
